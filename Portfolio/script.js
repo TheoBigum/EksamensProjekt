36aa0ecfc17ef14e5dfd0b2fecf1460196a1c4b6
TheoBigum <Theobigum@gmail.com>
2019-06-17 13:44:44 +0200

    finish1

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -3,3 +3,13 @@ window.addEventListener("load", sidenVises);
 function sidenVises() {
     console.log("sidenVises");
 }
+
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
